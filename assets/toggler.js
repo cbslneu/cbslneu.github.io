0eff9335e34a7f2f52cb1fd7fbf4fb6e6dbbe9c5
nmy2103 <yamane.n@northeastern.edu>
2022-09-06 13:46:58 -0400

    added revised figures

--- assets/toggler.js
+++ assets/toggler.js
@@ -1,4 +1,4 @@
-const toggler = document.querySelectorAll('[class$="directory"]');
+const toggler = document.querySelectorAll('[class$="directory"], [class$="label"]');
 
 const directoryClassName = {
   'fa-folder directory' : ['.directory', '.internal-label'],
@@ -13,7 +13,31 @@ const directoryClassName = {
   'a10-directory': ['.a10-directory', '.a10-label'],
   'a11-directory': ['.a11-directory', '.a11-label'],
   'ae4-directory': ['.ae4-directory', '.ae4-label'],
-  'ae6-directory': ['.ae6-directory', '.ae6-label']
+  'ae6-directory': ['.ae6-directory', '.ae6-label'],
+  't01-directory': ['.t01-directory', '.t01-label'],
+  't02-directory': ['.t02-directory', '.t02-label'],
+  't03-directory': ['.t03-directory', '.t03-label'],
+  't04-directory': ['.t04-directory', '.t04-label'],
+  't05-directory': ['.t05-directory', '.t05-label'],
+  't06-directory': ['.t06-directory', '.t06-label'],
+  't07-directory': ['.t07-directory', '.t07-label'],
+  't08-directory': ['.t08-directory', '.t08-label'],
+  't09-directory': ['.t09-directory', '.t09-label'],
+  't10-directory': ['.t10-directory', '.t10-label'],
+  't11-directory': ['.t11-directory', '.t11-label'],
+  't12-directory': ['.t12-directory', '.t12-label'],
+  't13-directory': ['.t13-directory', '.t13-label'],
+  't14-directory': ['.t14-directory', '.t14-label'],
+  't15-directory': ['.t15-directory', '.t15-label'],
+  't16-directory': ['.t16-directory', '.t16-label'],
+  't17-directory': ['.t17-directory', '.t17-label'],
+  't18-directory': ['.t18-directory', '.t18-label'],
+  't20-directory': ['.t20-directory', '.t20-label'],
+  't21-directory': ['.t21-directory', '.t21-label'],
+  't22-directory': ['.t22-directory', '.t22-label'],
+  't23-directory': ['.t23-directory', '.t23-label'],
+  't24-directory': ['.t24-directory', '.t24-label'],
+
 }
 
 for (let i = 0; i < toggler.length; i++) {
@@ -32,5 +56,3 @@ function directoryToggle(firstEle, secondEle) {
   directoryEle.find(firstEle).toggleClass('fa-folder-open');
   directoryEle.find(secondEle).toggleClass('font-weight-bold');
 }
-
-
